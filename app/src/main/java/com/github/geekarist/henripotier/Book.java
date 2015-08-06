3ce52474a3bc849c41c567b3a792608e4954cf53
Christophe Pelé <cpele@xebia.fr>
2015-08-06 13:54:27 +0200

    Make Book object immutable

--- app/src/main/java/com/github/geekarist/henripotier/Book.java
+++ app/src/main/java/com/github/geekarist/henripotier/Book.java
@@ -1,11 +1,16 @@
 package com.github.geekarist.henripotier;
 
-import android.graphics.Bitmap;
+public final class Book {
+    final String isbn;
+    final String title;
+    final Integer price;
+    final String cover;
 
-public class Book {
-    String isbn;
-    String title;
-    Integer price;
-    String cover;
+    public Book(String isbn, String title, Integer price, String cover) {
+        this.isbn = isbn;
+        this.title = title;
+        this.price = price;
+        this.cover = cover;
+    }
 }
 
