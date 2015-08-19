9372ee929c64889ab2e072f366023f5293df97c3
Christophe Pelé <cpele@xebia.fr>
2015-08-19 13:54:41 +0200

    Add listener on continueShopping Button

--- app/src/main/java/com/github/geekarist/henripotier/CartActivity.java
+++ app/src/main/java/com/github/geekarist/henripotier/CartActivity.java
@@ -1,13 +1,31 @@
 package com.github.geekarist.henripotier;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class CartActivity extends Activity {
+
+    @Bind(R.id.continue_shopping)
+    Button continueShoppingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart_list);
+
+        ButterKnife.bind(this);
+
+        continueShoppingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CartActivity.this, BookListActivity.class));
+            }
+        });
     }
 }
