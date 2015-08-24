275f270061b59d1dcb73c219c3fbed644da4e3aa
geekarist <christophe.pele@gmail.com>
2015-08-24 08:14:31 +0200

    WIP: implement CursorAdapter with SQLiteDatabase

--- app/src/main/java/com/github/geekarist/henripotier/CartAdapter.java
+++ app/src/main/java/com/github/geekarist/henripotier/CartAdapter.java
@@ -1,10 +1,13 @@
 package com.github.geekarist.henripotier;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.CursorAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,7 +19,7 @@ import java.util.List;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class CartAdapter extends BaseAdapter {
+public class CartAdapter extends CursorAdapter {
 
     private final List<Book> mCartItems = new ArrayList<>();
     private final Context mContext;
@@ -28,8 +31,14 @@ public class CartAdapter extends BaseAdapter {
     @Bind(R.id.cartItemImageView)
     ImageView mImageView;
 
-    public CartAdapter(Context context) {
+    private CartAdapter(Context context, Cursor cursor) {
+        super(context, cursor, false);
         this.mContext = context;
+    }
+
+    public static CartAdapter newInstance(Context context) {
+        Cursor cursor = new DatabaseHelper(context, null, null, 0).getWritableDatabase().rawQuery("SELECT * FROM book", null);
+        return new CartAdapter(context, cursor);
     }
 
     @Override
@@ -63,8 +72,40 @@ public class CartAdapter extends BaseAdapter {
         return view;
     }
 
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
+
     public void add(Book purchasedBook) {
         mCartItems.add(purchasedBook);
         notifyDataSetChanged();
+    }
+
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+        public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+            super(context, name, factory, version);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE book (id INTEGER PRIMARY KEY, isbn TEXT, title TEXT, price INTEGER, cover TEXT");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS book");
+        }
+
+        public void insert(Book book) {
+            SQLiteDatabase db = getWritableDatabase();
+            db.execSQL("INSERT INTO book VALUES (id, isbn, title, price, cover) SET (%d, %s, %s, %d, %s)");
+            // TODO
+        }
     }
 }
