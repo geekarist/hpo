f562c21844b932fe2590a0dc4a336c1dc976304e
Christophe Pelé <cpele@xebia.fr>
2015-09-14 11:24:32 +0200

    Test CatalogActivity UI behaviour
    - Check that a Book list is displayed
    - Check that when choosing a book, the Cart is displayed

---
+++ app/src/androidTest/java/com/github/geekarist/henripotier/CatalogActivityTest.java
@@ -0,0 +1,36 @@
+package com.github.geekarist.henripotier;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.hamcrest.Matchers;
+
+public class CatalogActivityTest extends ActivityInstrumentationTestCase2<CatalogActivity> {
+    private CatalogActivity mActivity;
+
+    public CatalogActivityTest() {
+        super(CatalogActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        mActivity = getActivity();
+    }
+
+    public void testShouldDisplayListOfBooks() {
+        Espresso.onData(Matchers.instanceOf(Book.class))
+                .inAdapterView(Matchers.allOf(ViewMatchers.withId(R.id.list), ViewMatchers.isDisplayed()))
+                .atPosition(0)
+                .onChildView(ViewMatchers.withId(R.id.book_selection_button))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+                .perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withId(R.id.cart_list))
+                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
+}
