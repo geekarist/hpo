c3584a4c482a0c7505da10f5ad8f5a9e3a6c50b9
Christophe Pelé <cpele@xebia.fr>
2015-09-16 22:05:59 +0200

    Wait 3 seconds before checking catalog

--- app/src/androidTest/java/com/github/geekarist/henripotier/CatalogActivityTest.java
+++ app/src/androidTest/java/com/github/geekarist/henripotier/CatalogActivityTest.java
@@ -3,11 +3,15 @@ package com.github.geekarist.henripotier;
 import android.database.Cursor;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.Espresso;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
 
+import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 
 public class CatalogActivityTest extends ActivityInstrumentationTestCase2<CatalogActivity> {
@@ -24,13 +28,16 @@ public class CatalogActivityTest extends ActivityInstrumentationTestCase2<Catalo
         mActivity = getActivity();
     }
 
-    public void testShouldDisplayListOfBooks() {
+    public void testShouldDisplayListOfBooks() throws InterruptedException {
+        Espresso.onView(ViewMatchers.isRoot()).perform(new WaitFor(3000));
+
         Espresso.onData(Matchers.instanceOf(Book.class))
                 .inAdapterView(Matchers.allOf(ViewMatchers.withId(R.id.list), ViewMatchers.isDisplayed()))
                 .atPosition(0)
                 .onChildView(ViewMatchers.withId(R.id.book_selection_button))
                 .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
                 .perform(ViewActions.click());
+
 
         Espresso.onView(ViewMatchers.withId(R.id.cart_list))
                 .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
@@ -41,5 +48,29 @@ public class CatalogActivityTest extends ActivityInstrumentationTestCase2<Catalo
                 .onChildView(ViewMatchers.withId(R.id.book_selection_button))
                 .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
                 .perform(ViewActions.click());
+    }
+
+    private static class WaitFor implements ViewAction {
+
+        private int time;
+
+        public WaitFor(int time) {
+            this.time = time;
+        }
+
+        @Override
+        public Matcher<View> getConstraints() {
+            return ViewMatchers.isRoot();
+        }
+
+        @Override
+        public String getDescription() {
+            return "wait for " + time + " seconds";
+        }
+
+        @Override
+        public void perform(UiController uiController, View view) {
+            uiController.loopMainThreadForAtLeast(time);
+        }
     }
 }
