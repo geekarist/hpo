30ae630f3b2f0d12d45ae4563d9bd37ca5f2b99a
Christophe Pelé <cpele@xebia.fr>
2015-11-12 19:19:21 +0100

    Upload spoon report after build

---
+++ bin/save-reports.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eu
+
+test -d app/build/spoon
+tar zcf app/build/spoon.tgz app/build/spoon
+ncftpput \
+	-u $FTP_USER \
+   	-p $FTP_PASSWORD \
+	$FTP_HOST \
+	/home/$FTP_USER app/build/spoon.tgz
