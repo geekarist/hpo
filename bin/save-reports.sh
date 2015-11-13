f5089a1df050797236f8bbdd07052bb54647f041
geekarist <christophe.pele@gmail.com>
2015-11-13 08:32:40 +0100

    Use `tree` to debug save-reports

--- bin/save-reports.sh
+++ bin/save-reports.sh
@@ -2,13 +2,15 @@
 
 set -eu
 
-if test -d app/build/spoon ; then
-	tar zcf app/build/spoon.tgz app/build/spoon
+tree
+
+if test -d ./app/build/spoon ; then
+	tar zcf ./app/build/spoon.tgz ./app/build/spoon
 	ncftpput \
 		-u $FTP_USER \
 		-p $FTP_PASSWORD \
 		$FTP_HOST \
-		/home/$FTP_USER app/build/spoon.tgz
+		/home/$FTP_USER ./app/build/spoon.tgz
 	echo Test report has been uploaded
 else
 	echo No report to upload
