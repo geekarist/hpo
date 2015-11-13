31a7e4f0bff90d01b18cc3889e4eb01d074e0096
geekarist <christophe.pele@gmail.com>
2015-11-13 07:39:28 +0100

    Don't upload if no reports

--- bin/save-reports.sh
+++ bin/save-reports.sh
@@ -2,10 +2,14 @@
 
 set -euv
 
-test -d app/build/spoon
-tar zcf app/build/spoon.tgz app/build/spoon
-ncftpput \
-	-u $FTP_USER \
-   	-p $FTP_PASSWORD \
-	$FTP_HOST \
-	/home/$FTP_USER app/build/spoon.tgz
+if test -d app/build/spoon ; then
+	tar zcf app/build/spoon.tgz app/build/spoon
+	ncftpput \
+		-u $FTP_USER \
+		-p $FTP_PASSWORD \
+		$FTP_HOST \
+		/home/$FTP_USER app/build/spoon.tgz
+	echo Test report has been uploaded
+else
+	echo No report to upload
+fi
