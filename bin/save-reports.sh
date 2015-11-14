f4f90f1379ceb0b970cc9d6b80d4f711527b89f1
geekarist <christophe.pele@gmail.com>
2015-11-14 09:43:09 +0100

    Quote connection variables

--- bin/save-reports.sh
+++ bin/save-reports.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -eux
+set -eu
 
 tree -d
 
 if test -d ./app/build/spoon ; then
 	tar zcf ./app/build/spoon.tgz ./app/build/spoon
-	ncftpput -u $FTP_USER -p $FTP_PASSWORD $FTP_HOST /home/$FTP_USER ./app/build/spoon.tgz
+	ncftpput -u "$FTP_USER" -p "$FTP_PASSWORD" "$FTP_HOST" /home/"$FTP_USER" ./app/build/spoon.tgz
 	echo Test report has been uploaded
 else
 	echo No report to upload
